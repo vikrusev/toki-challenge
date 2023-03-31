fdfbe4ea4fd9ed425c4cf59970ba6ea9fe064f8d
Viktor Rusev <vikrusev257@gmail.com>
2023-03-31 15:08:17 +0300

    main function modifications
    switched to Typescript, added debug configuration w/ hot-reload

---
+++ functions/index.ts
@@ -0,0 +1,19 @@
+const { Storage: Storage2 } = require("@google-cloud/storage");
+
+const functions = async (req: any, res: any) => {
+  // Instantiate a new client using the service account key file
+  const storage = new Storage2({
+    keyFilename: "/secrets/toki-service-account",
+  });
+
+  // Use the client to access Google Cloud Storage
+  const bucketName = "toki-take-home.appspot.com";
+  const [files] = await storage.bucket(bucketName).getFiles();
+  console.log(`Retrieved all files in ${bucketName}:`);
+
+  res.send(
+    `Available files: \r\n ${files.map((f: any) => f.name).join("\r\n")}`
+  );
+};
+
+export { functions };
