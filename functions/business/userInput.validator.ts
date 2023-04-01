7b6f1dc5521fb40f69730d6616c52b91aedc0ff7
Viktor Rusev <vikrusev257@gmail.com>
2023-04-01 19:40:15 +0300

    update UserInputValidator to parse a string-encoded array

--- functions/business/userInput.validator.ts
+++ functions/business/userInput.validator.ts
@@ -4,18 +4,25 @@ import { UserInput } from "../models/UserInput.model";
  * Checks if the user input is valid or not
  * TODO add additional checks for year, month and day
  * TODO e.g. they should be positive integers
- * @param {UserInput} userInput consists of requested time and eventually metering point ids
+ * @param {UserInput} param0 consists of requested time and eventually metering point ids
  * @returns {boolean} if the input is valid or not
  */
-const isUserInputValid = (userInput: UserInput): boolean => {
-  return !(
-    !userInput.year || // year must be provided
-    (userInput.year && userInput.day && !userInput.month) || // year and day must have month
-    isNaN(Number(userInput.year)) || // year should be a number|
-    isNaN(Number(userInput.month)) || // month should be a number|
-    isNaN(Number(userInput.day)) || // day should be a number|
-    // all meteringPointIds should be number
-    userInput.meteringPointIds?.some((id) => isNaN(Number(id)))
+const isUserInputValid = ({
+  year,
+  month,
+  day,
+  meteringPointIds,
+}: UserInput): boolean => {
+  const pointIds = meteringPointIds?.split(",");
+
+  return !Boolean(
+    !year || // year must be provided
+      isNaN(Number(year)) || // year should be a number
+      Boolean(year && day && !month) || // year and day must have month
+      Boolean(month && isNaN(Number(month))) || // month should be a number
+      Boolean(day && isNaN(Number(day))) || // day should be a number
+      // all meteringPointIds should be number
+      pointIds?.some((id) => isNaN(Number(id)))
   );
 };
 
