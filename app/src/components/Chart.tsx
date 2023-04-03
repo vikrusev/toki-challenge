e7243e9a6ba82147020e6689ba8dd0e69d502f2c
Viktor Rusev <vikrusev257@gmail.com>
2023-04-03 14:39:29 +0300

    React: simple Dropdown component and hooks usage

--- app/src/components/Chart.tsx
+++ app/src/components/Chart.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   LineChart,
   Line,
@@ -8,43 +9,70 @@ import {
   Legend,
 } from "recharts";
 
+import { ClientResponse } from '../../../common/data.types'
+import Dropdown from "./Dropdown";
+
 interface IProps {
   title: string;
 }
 
+const createArray = (untilNumber: number) => Array.from({ length: untilNumber }, (_, i) => String(i + 1).padStart(2, '0'))
+
 const Chart: React.FC<IProps> = ({ title }: IProps) => {
-  const data = [
-    { name: 'Jan', value: 100 },
-    { name: 'Feb', value: 200 },
-    { name: 'Mar', value: 150 },
-    { name: 'Apr', value: 350 },
-    { name: 'May', value: 250 },
-    { name: 'Jun', value: 450 },
-    { name: 'Jul', value: 400 },
-    { name: 'Aug', value: 300 },
-    { name: 'Sep', value: 500 },
-    { name: 'Oct', value: 550 },
-    { name: 'Nov', value: 700 },
-    { name: 'Dec', value: 800 },
-  ];
+  const years = ["2022", "2023"];
+  const months = createArray(12);
+  const days = createArray(31);
+  
+  const [data, setData] = useState<ClientResponse>()
+
+  const [year, setYear] = useState<string>('2022')
+  const [month, setMonth] = useState<string>('04')
+  const [day, setDay] = useState<string>('01')
+
+  useEffect(() => {
+    setData({ 
+      pricesData: [
+        { datetime: 'Jan', price: 100 },
+        { datetime: 'Feb', price: 200 },
+        { datetime: 'Mar', price: 150 },
+        { datetime: 'Apr', price: 350 },
+        { datetime: 'May', price: 250 },
+        { datetime: 'Jun', price: 450 },
+        { datetime: 'Jul', price: 400 },
+        { datetime: 'Aug', price: 300 },
+        { datetime: 'Sep', price: 500 },
+        { datetime: 'Oct', price: 550 },
+        { datetime: 'Nov', price: 700 },
+        { datetime: 'Dec', price: 800 },
+      ],
+      usageData: []
+    })
+  }, [year, month, day])
 
   return (
     <>
       <h1>{title}</h1>
 
-      <LineChart width={600} height={300} data={data}>
-        <XAxis dataKey="name" />
-        <YAxis />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Legend />
-        <Line
-          type="monotone"
-          dataKey="value"
-          stroke="#8884d8"
-          activeDot={{ r: 8 }}
-        />
-      </LineChart>
+      <Dropdown label="year" values={years} onChange={setYear}/>
+      <Dropdown label="month" values={months} onChange={setMonth}/>
+      <Dropdown label="day" values={days} onChange={setDay}/>
+
+      {
+        data?.pricesData && 
+        <LineChart width={600} height={300} data={data.pricesData}>
+          <XAxis dataKey="datetime" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="price"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      }
     </>
   );
 };
