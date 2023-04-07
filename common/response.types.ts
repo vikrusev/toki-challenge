36fe093fe6b1f7d87f6d3e659e848be6f50fd2cb
Viktor Rusev <vikrusev257@gmail.com>
2023-04-08 00:10:40 +0300

    simplify ClientResponse type

--- common/response.types.ts
+++ common/response.types.ts
@@ -1,10 +1,7 @@
-import {
-    AggregatedData,
-    UnifiedPriceUsage,
-} from "../functions/types/information.types";
-
 // clean data to be used directly on the frontend layer
-export interface ClientResponse extends UnifiedPriceUsage {
+export interface ClientResponse {
+    datetime: number;
+    datetimeKey: number;
     electricityPrice: number;
-    datetimeKey: AggregatedData["datetimeKey"];
+    [price: string]: number;
 }
